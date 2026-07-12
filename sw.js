75b8b22cf44a73bcbcdd73738629bad64f816bb1
43515078 <52327797+43515078@users.noreply.github.com>
2026-07-11 23:59:50 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "avigenio-cobb-v4-6";
+const CACHE = "avigenio-cobb-v4-7-1";
 const ASSETS = ["/", "/index.html", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", event => {
